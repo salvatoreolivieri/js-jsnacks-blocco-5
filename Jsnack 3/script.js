d29cfe6d9a802a57633898aa475e0730c0aa8144
salvatoreolivieri <Salvatore.olivieri07@gmail.com>
2022-04-28 16:08:22 +0200

    Terzo Jsnack completato

--- Jsnack 3/script.js
+++ Jsnack 3/script.js
@@ -1,3 +1,12 @@
 /*
 Scrivi una funzione che accetti una stringa come argomento e la ritorni girata (es. Ciao -> oaiC)
 */
+
+const word = "anna";
+
+giraparola(word);
+console.log(giraparola(word));
+
+function giraparola(str) {
+  return str.split("").reverse().join("");
+}
