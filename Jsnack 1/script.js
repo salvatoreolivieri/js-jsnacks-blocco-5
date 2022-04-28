fed5eb84ccad2d16e4744c7ea7862a439600b83a
salvatoreolivieri <Salvatore.olivieri07@gmail.com>
2022-04-28 15:43:56 +0200

    Primo Jsnack completato

--- Jsnack 1/script.js
+++ Jsnack 1/script.js
@@ -1,4 +1,82 @@
 /*
-Crea un array di 10 oggetti che rappresentano una zucchina, indicando per ognuna varietà, peso e lunghezza.
+Crea un array di 10 oggetti che rappresentano una zucchina, indicando per ognuna varietà, kg e lunghezza.
 Calcola quanto pesano tutte le zucchine.
 */
+
+const zucchine = [
+
+  {
+    varietà: "nera",
+    kg: 2,
+    lunghezza: "30cm",
+  },
+
+  {
+    varietà: "romanesco",
+    kg: 4,
+    lunghezza: "20cm",
+  },
+
+  {
+    varietà: "fiorentina",
+    kg: 1,
+    lunghezza: "22cm",
+  },
+  
+  {
+    varietà: "napoletana",
+    kg: 3,
+    lunghezza: "27cm",
+  },
+  
+  {
+    varietà: "tonde",
+    kg: 2,
+    lunghezza: "25cm",
+  },
+  
+  {
+    varietà: "trombetta",
+    kg: 5,
+    lunghezza: "40cm",
+  },
+  
+  {
+    varietà: "ortolano",
+    kg: 2,
+    lunghezza: "24cm",
+  },
+  
+  {
+    varietà: "siciliano",
+    kg: 3,
+    lunghezza: "18cm",
+  },
+  
+  {
+    varietà: "bianca",
+    kg: 7,
+    lunghezza: "14cm",
+  },
+  
+  {
+    varietà: "rigata",
+    kg: 3,
+    lunghezza: "32cm",
+  }
+
+]
+
+
+let pesoTot = 0;
+
+for (const index in zucchine) {
+
+  const peso = zucchine[index].kg;
+  console.log(peso);
+
+  pesoTot += peso;
+
+}
+
+console.log(pesoTot);
